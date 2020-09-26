5da3c2fc8f8bba77fab11dfc449fcf600d72499f
hkirschke <33073460+hkirschke@users.noreply.github.com>
2020-09-26 11:00:35 -0300

    Update fisica
    Criação propriedades Fisica.cs e criação login controller

--- SisAdot/Models/Pessoa/Fisica.cs
+++ SisAdot/Models/Pessoa/Fisica.cs
@@ -7,5 +7,39 @@ namespace SisAdot.Models.Pessoa
 {
   public class Fisica : Usuario
   {
+    public string nomePessoa;
+
+
+    private string _nomePessoa;
+
+    public string NomePessoa
+    {
+      get { return _nomePessoa; }
+      set { _nomePessoa = value; }
+    }
+     
+    private string _cpf;
+
+    public string CPF
+    {
+      get { return _cpf; }
+      set { _cpf = value; }
+    }
+
+    private string _rgr;
+
+    public string RG
+    {
+      get { return _rgr; }
+      set { _rgr = value; }
+    }
+
+    private DateTime _dataNascimento;
+
+    public  DateTime DataNascimento
+    {
+      get { return _dataNascimento; }
+      set { _dataNascimento = value; }
+    } 
   }
 }
