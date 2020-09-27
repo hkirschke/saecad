a193f90dcfd459331cb89d587dd46579259fb7fc
hkirschke <33073460+hkirschke@users.noreply.github.com>
2020-09-27 10:53:02 -0300

    Retirada de o auth e alteração usuário
    Retirada do auth

--- SisAdot/Controllers/HomeController.cs
+++ SisAdot/Controllers/HomeController.cs
@@ -1,41 +1,15 @@
 ﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web.Mvc;
-
-using Google.Apis.Auth.OAuth2.Mvc;
-using Google.Apis.Drive.v3;
-using Google.Apis.Services;
- 
-using SisAdot.OAuth;
+using System.Web.Mvc; 
 
 namespace SisAdot.Controllers
 {
   public class HomeController : Controller
   {
-    public async Task<ActionResult> IndexAsync(CancellationToken cancellationToken)
+    public ActionResult Index()
     {
-      var result = await new AuthorizationCodeMvcApp(this, new AppFlowMetadata()).
-          AuthorizeAsync(cancellationToken);
-
-      if (result.Credential != null)
-      {
-        var service = new DriveService(new BaseClientService.Initializer
-        {
-          HttpClientInitializer = result.Credential,
-          ApplicationName = "SisAdot"
-        });
-
-        // YOUR CODE SHOULD BE HERE..
-        // SAMPLE CODE:
-        var list = await service.Files.List().ExecuteAsync();
-        ViewBag.Message = "FILE COUNT IS: " + list.Files.Count();
-        return View();
-      }
-      else
-      {
-        return new RedirectResult(result.RedirectUri);
-      }
+      return View();
     }
   }
 }
--- SisAdot/Models/Pessoa/Usuario.cs
+++ SisAdot/Models/Pessoa/Usuario.cs
@@ -7,11 +7,46 @@ using System.Web;
 namespace SisAdot.Models
 {
   public class Usuario
-  {
-    public Guid IdUsuario;
-    public PerfilUsuario PerfilUsuario;
-    public string login;
-    public string senha;
-    public string email;
+  { 
+
+    private Guid _idUsuario;
+
+    public Guid IdUsuario
+    {
+      get { return _idUsuario; }
+      set { _idUsuario = value; }
+    }
+
+    private string _perfilUsuario;
+
+    public string PerfilUsuario
+    {
+      get { return _perfilUsuario; }
+      set { _perfilUsuario = value; }
+    }
+
+    private string _login;
+
+    public string Login
+    {
+      get { return _login; }
+      set { _login = value; }
+    }
+
+    private string _senha;
+
+    public string Senha
+    {
+      get { return _senha; }
+      set { _senha = value; }
+    }
+     
+    private string _email;
+
+    public string Email
+    {
+      get { return _email; }
+      set { _email = value; }
+    } 
   }
 }
