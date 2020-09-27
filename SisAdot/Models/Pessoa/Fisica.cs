c62a664653c98e1973e8a708f3b717cec710ab7a
hkirschke <33073460+hkirschke@users.noreply.github.com>
2020-09-27 11:13:36 -0300

    update

--- SisAdot/Models/Pessoa/Fisica.cs
+++ SisAdot/Models/Pessoa/Fisica.cs
@@ -6,40 +6,12 @@ using System.Web;
 namespace SisAdot.Models.Pessoa
 {
   public class Fisica : Usuario
-  {
-    public string nomePessoa;
-
-
-    private string _nomePessoa;
-
-    public string NomePessoa
-    {
-      get { return _nomePessoa; }
-      set { _nomePessoa = value; }
-    }
+  { 
+    public string NomePessoa { get; set; } 
+    public string CPF { get; set; }
      
-    private string _cpf;
-
-    public string CPF
-    {
-      get { return _cpf; }
-      set { _cpf = value; }
-    }
-
-    private string _rg;
-
-    public string RG
-    {
-      get { return _rg; }
-      set { _rg = value; }
-    }
-
-    private DateTime _dataNascimento;
-
-    public  DateTime DataNascimento
-    {
-      get { return _dataNascimento; }
-      set { _dataNascimento = value; }
-    } 
+    public string RG { get; set; }
+     
+    public  DateTime DataNascimento { get; set; }
   }
 }
--- SisAdot/Models/Pessoa/Usuario.cs
+++ SisAdot/Models/Pessoa/Usuario.cs
@@ -1,52 +1,21 @@
 ﻿using SisAdot.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace SisAdot.Models
 {
+  [Table("Usuario")]
   public class Usuario
-  { 
-
-    private Guid _idUsuario;
-
-    public Guid IdUsuario
-    {
-      get { return _idUsuario; }
-      set { _idUsuario = value; }
-    }
-
-    private string _perfilUsuario;
-
-    public string PerfilUsuario
-    {
-      get { return _perfilUsuario; }
-      set { _perfilUsuario = value; }
-    }
-
-    private string _login;
-
-    public string Login
-    {
-      get { return _login; }
-      set { _login = value; }
-    }
-
-    private string _senha;
-
-    public string Senha
-    {
-      get { return _senha; }
-      set { _senha = value; }
-    }
-     
-    private string _email;
-
-    public string Email
-    {
-      get { return _email; }
-      set { _email = value; }
-    } 
+  {
+    [Key] 
+    public Guid IdUsuario { get; set; }
+    public PerfilUsuario PerfilUsuario { get; set; }
+    public string Login { get; set; }
+    public string Senha { get; set; }
+    public string Email { get; set; }
   }
 }
